cd6bb2fd146b83c8a0b7c0194519d0a51eb50c2a
Ahmad Hammoud <124024445+moody00au@users.noreply.github.com>
2024-02-11 16:08:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,9 @@ import json
 # Retrieve your OpenAI API key from Streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Google Cloud Text-to-Speech client setup with Streamlit secrets
-google_credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["GOOGLE_CLOUD_CREDENTIALS"]
-)
-google_tts_client = texttospeech.TextToSpeechClient(credentials=google_credentials)
-
+# Google Cloud Text-to-Speech client setup
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "https://github.com/moody00au/ArabicTTS/edit/main\tts-in-arabic-39d278d1f6b4.json"
+google_tts_client = texttospeech.TextToSpeechClient()
 def add_diacritics(text):
     try:
         response = openai.ChatCompletion.create(
